1a982241bc08ddc810a10997bebc133e985b23b3
Kingsley Onyensoh <onyensoh1987@hotmail.com>
2025-03-08 11:25:59 +0000

    Make temporary solution to screen switching

--- src/views/YearView/YearView.tsx
+++ src/views/YearView/YearView.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 function YearView({ ragloadState, screenState }) {
+	// Temporary function. Move to CurrentYearBar.tsx when it's been created
+	function switchToMonthView() {
+		screenState.setScreen(0);
+	}
+
 	return (
 		<div id="year-view">
-			<div id="year-view__current-year-bar">Current Year Bar</div>
+			<div id="year-view__current-year-bar">
+				Current Year Bar <span onClick={switchToMonthView}>{">"}</span>
+			</div>
 			<div id="year-view__month-cards">Month Cards</div>
 			<div id="year-view__tool-bar">Tool Bar</div>
 		</div>
